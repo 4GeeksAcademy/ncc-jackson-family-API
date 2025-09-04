0ad92fd5b3f9e146b3f60ea7cdd97702979472ba
Nicolas Carrillo <n.carrillocofr@gmail.com>
2025-09-04 16:15:03 +0000

    fix family static api flask

--- src/datastructures.py
+++ src/datastructures.py
@@ -4,11 +4,13 @@ Update this file to implement the following already declared methods:
 - delete_member: Should delete a member from the self._members list
 - get_member: Should return a member from the self._members list
 """
+from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
+        # example list of members
         self._members = [
             {
                 "id": self._generate_id(),
@@ -16,30 +18,56 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
-    # This method generates a unique incremental ID
+    # read-only: Use this method to generate random members ID's when adding members into the list
     def _generate_id(self):
         generated_id = self._next_id
         self._next_id += 1
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        # Si no tiene ID, generar uno
+        if 'id' not in member:
+            member['id'] = self._generate_id()
+        
+        # Agregar last_name
+        member['last_name'] = self.last_name
+        
+        # Agregar el miembro
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        # Buscar y eliminar el miembro
+        for i, member in enumerate(self._members):
+            if member["id"] == id:
+                del self._members[i]
+                return True
+        return False
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
-
-    # This method is done, it returns a list with all the family members
+        # Retornar UN miembro, no una lista
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
+        
+    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
